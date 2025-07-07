2b12aeaa919d765d8d1aaf7dbb99e14d9aa596dc
Gihan <gihanc302@gmail.com>
2025-07-08 00:01:47 +0530

    Home done 95%

--- auto_care_frontend/src/app/page.js
+++ auto_care_frontend/src/app/page.js
@@ -1,6 +1,7 @@
 import { Hero } from './user/components/homepage/hero';
-import {WhyAutoCare}  from './user/components/homepage/whyAutoCare';
+import { WhyAutoCare } from './user/components/homepage/whyAutoCare';
 import { VehicleTypeSelector } from './user/components/homepage/vehicleTypeSlector';
+import FeaturedTools from './user/components/homepage/featuredTools';
 import BuyingPowerCal from './user/components/homepage/buyingPowerCal';
 
 export default function HomePage() {
@@ -10,6 +11,7 @@ export default function HomePage() {
       <VehicleTypeSelector />
       <WhyAutoCare />
       <BuyingPowerCal />
+      <FeaturedTools />
     </>
   );
 }
