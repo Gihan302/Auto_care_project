21a5e12321e085923db0f6d0ae06a00149210eed
Gihan <gihanc302@gmail.com>
2025-07-21 21:44:25 +0530

    With folder str

--- auto_care_frontend/src/app/layout.js
+++ auto_care_frontend/src/app/layout.js
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./page.module.css"; // adjust path if needed
+import "./page.module.css";
 
 import Header from "./layout/header/page";
 import Footer from "./layout/footer/page";
