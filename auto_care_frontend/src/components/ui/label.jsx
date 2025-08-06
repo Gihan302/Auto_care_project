9d8b7e1c8c77c44cc674b349ca85962f3be630d0
Isuru35196 <isurugayantha35196@gmail.com>
2025-08-06 09:23:55 +0530

    login bug fixed
    login bug fixed

--- auto_care_frontend/src/components/ui/label.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function Label({ htmlFor, children, className = '' }) {
-  return (
-    <label htmlFor={htmlFor} className={`block text-sm font-medium mb-1 ${className}`}>
-      {children}
-    </label>
-  );
-}
